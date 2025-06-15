e19a871dbd2cf281efdbb09f7423a67b7ae87366
caua <marizcaua0@gmail.com>
2025-06-15 17:06:57 -0300

    Atualizacao arquivos

--- app/src/main/java/com/example/projetotenisbr/InitialScreen.kt
+++ app/src/main/java/com/example/projetotenisbr/InitialScreen.kt
@@ -1,7 +1,11 @@
 package com.example.projetotenisbr
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +19,34 @@ class InitialScreen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+
+    fun enterMainScreen(view: View){
+        val emailUser = findViewById<EditText>(R.id.emailUser).text.toString().trim();
+        val passUser = findViewById<EditText>(R.id.passUser).text.toString().trim();
+        if(emailUser.isEmpty()){
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Aviso")
+                .setMessage("Por favor, insira seu email.")
+                .setPositiveButton("Ok"){dialog, _ -> dialog.dismiss()}
+                .create()
+
+            alert.show();
+        }
+        else if(passUser.isEmpty()){
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Aviso")
+                .setMessage("Por favor, insira sua senha.")
+                .setPositiveButton("Ok"){dialog, _ -> dialog.dismiss()}
+                .create()
+
+            alert.show();
+        }
+        else{
+            val intent =  Intent(this, MainScreen::class.java);
+            startActivity(intent);
         }
     }
 }
