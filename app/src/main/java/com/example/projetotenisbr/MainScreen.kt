11d5ee6350ec1b3a4a1bdd5cb8ed0ecc5b1b6f61
caua <marizcaua0@gmail.com>
2025-06-24 17:40:10 -0300

    Mudanca na home

--- app/src/main/java/com/example/projetotenisbr/MainScreen.kt
+++ app/src/main/java/com/example/projetotenisbr/MainScreen.kt
@@ -1,20 +1,56 @@
 package com.example.projetotenisbr
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_screen)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.home_menu -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.profile_menu -> {
+                Toast.makeText(this, "Sua conta", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.carrinho_menu -> {
+                Toast.makeText(this, "Seu carrinho", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.config_menu -> {
+                Toast.makeText(this, "Configuracoes", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
